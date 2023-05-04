3cde1df344264c70aec0ccf2818ca1f71390e712
josh20122 <jamesjoshua20122@gmail.com>
2023-05-04 07:17:32 +0300

    register

---
+++ app/Http/Controllers/ApiLoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class ApiLoginController extends Controller
+{
+    public function index(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return self::apiLogin($user);
+    }
+
+
+    public static function apiLogin($user)
+    {
+        return $user->createToken($user->email)->plainTextToken;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApiLoginController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\SmsWebhook;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [ApiLoginController::class, 'index']);
 
 
 Route::post('/sms-webhook', [SmsWebhook::class, 'register']);
